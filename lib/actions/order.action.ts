23f2537679ad31d941bdb0d832f3565476bb0297
dzx <dzxdetianxuan@outlook.com>
2025-06-21 00:34:21 +0800

    completed to P69

--- lib/actions/order.action.ts
+++ lib/actions/order.action.ts
@@ -5,6 +5,10 @@ import { formatError } from "../utils"
 import { auth } from "@/auth"
 import { getMyCart } from "./cart.actions"
 import { getUserById } from "./user.actions"
+import { insertOrderItemSchema, insertOrderSchema, shippingAddressSchema } from "../validator"
+import { prisma } from "@/db/prisma"
+import { CartItem } from "@/types"
+import { redirect } from "next/dist/server/api-utils"
 
 export async function createOrder() {
     try{
@@ -16,6 +20,68 @@ export async function createOrder() {
         if(!userId) throw new Error('User not found')
         
         const user = await getUserById(userId)
+        
+
+        if(!cart || cart.items.length === 0) {
+            return {
+                success: false, message: 'Your cart is empty', redirectTo: '/cart'
+            }
+        }
+        if(!user.address) {
+            return {
+                success: false, message: 'No shipping address', redirectTo: '/shipping-address'
+            }
+        }
+        if(!user.paymentMethod) {
+            return {
+                success: false, message: 'No payment method', redirectTo: '/payment-method'
+            }
+        }
+
+        // Create order object
+        const order = insertOrderSchema.parse({
+            userId: user.id,
+            shippingAddress: user.address,
+            paymentMethod: user.paymentMethod,
+            itemsPrice: cart.itemsPrice,
+            shippingPrice: cart.shippingPrice,
+            taxPrice: cart.taxPrice,
+            totalPrice: cart.totalPrice,
+        })
+
+        // Create a transaction to create order and order items in database
+        const insertedOrderId = await prisma.$transaction(async (tx) => {
+            // Create order
+            const insertedOrder = await tx.order.create({data: order})
+            // Create order items from the cart items
+            for (const item of cart.items as CartItem[]) {
+                await tx.orderItem.create({
+                    data: {
+                        ...item,
+                        price: item.price,
+                        orderId: insertedOrder.id
+                    }
+                })
+            }
+
+            // Clear cart
+            await tx.cart.update({
+                where: {id: cart.id},
+                data: {
+                    items: [],
+                    totalPrice: 0,
+                    taxPrice: 0,
+                    shippingPrice: 0,
+                    itemsPrice: 0,
+                }
+            })
+
+            return insertedOrder.id
+        })
+
+        if(!insertedOrderId) throw new Error('Order not created')
+        
+        return{success: true,message: 'Order created', redirectTo: `/order/${insertedOrderId}`}
         
     }catch(error) {
         if(isRedirectError(error)) throw error
