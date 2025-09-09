ef46d19f098266c0b6c16d9c5c464a912ae79657
dzx <dzxdetianxuan@outlook.com>
2025-09-09 22:03:15 +0800

    暂存版本

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+import { updateOrderToPaid } from "@/lib/actions/order.action";
+
+export async function POST(req: NextRequest) {
+  // Build the webhook
+  const event = await Stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get("stripe-signature") as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  );
+
+  // Check for successful payment
+  if (event.type === "charge.succeeded") {
+    const { object } = event.data;
+
+    // Update order status
+    await updateOrderToPaid({
+      orderId: object.metadata.orderId,
+      paymentResult: {
+        id: object.id,
+        status: "COMPLETED",
+        email_address: object.billing_details.email,
+        pricePaid: (object.amount / 100).toFixed(),
+      },
+    });
+
+    return NextResponse.json({
+      message: "updateOrderToPaid was successful",
+    });
+  }
+
+  return NextResponse.json({
+    message: "event is not charge.succeeded",
+  });
+}
